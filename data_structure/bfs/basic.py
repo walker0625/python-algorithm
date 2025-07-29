63399243fd8fde6e0d55cde281720f881d63a81a
walker0625 <jeonminwoo0625@gmail.com>
2025-07-30 08:31:05 +0900

    bfs max depth

--- data_structure/bfs/basic.py
+++ data_structure/bfs/basic.py
@@ -21,3 +21,25 @@ def bfs(root):
             q.append(cur_node.right)
     
     return visited      
+
+def max_depth(root):
+    
+    max_depth = 0
+    
+    if root is None:
+        return max_depth
+    
+    q = deque()
+    q.append((root, 1)) # 깊이 1부터 시작
+    
+    while q:
+        cur_node, cur_depth = q.popleft()
+        
+        max_depth = max(max_depth, cur_depth)
+        
+        if cur_node.left:
+            q.append(cur_node.left, cur_depth + 1)
+        if cur_node.right:
+            q.append(cur_node.right, cur_depth + 1)
+            
+    return max_depth
