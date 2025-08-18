7f0707363549cc31e0003b73c0b3a563abb1cd91
walker0625 <jeonminwoo0625@gmail.com>
2025-08-19 08:14:57 +0900

    dp / fibonacci bottom up 방식 추가

--- category/dp/fibonacci.py
+++ category/dp/fibonacci.py
@@ -1,14 +1,28 @@
 memo = {}
 
-def fibo(n):
+def fibo_top_down(n):
     
     if n == 1 or n == 2:
         return 1
     
     if n not in memo:
-        memo[n] = fibo(n-1) + fibo(n-2)    
+        memo[n] = fibo_top_down(n-1) + fibo_top_down(n-2)    
         
-    return memo[n]
+    return memo[n] # 저장된 값을 return 
 
-print(fibo(7))
+print(fibo_top_down(7))
 print(memo)
+
+##############################################################
+
+memo2 = {1: 1, 2: 1}
+
+def fibo_bottom_up(n):
+    
+    for i in range(3, n+1):
+        memo2[i] = memo2[i-1] + memo2[i-2]
+        
+    return memo2[n] # 저장된 값을 return 
+
+print(fibo_bottom_up(7))
+print(memo2)
